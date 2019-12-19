865399077ed8cf16ca4bb0e4a2519da113987344
tryhard-lyy <1094169189@qq.com>
2019-12-19 17:26:24 +0800

    update

--- src/main/java/com/mmall/dao/CategoryMapper.java
+++ src/main/java/com/mmall/dao/CategoryMapper.java
@@ -1,6 +1,10 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Category;
+import com.mmall.pojo.Product;
+
+import java.util.List;
+
 
 public interface CategoryMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +18,7 @@ public interface CategoryMapper {
     int updateByPrimaryKeySelective(Category record);
 
     int updateByPrimaryKey(Category record);
+
+    List<Category> selectCategoryChildrenByParentId(int parentId);
+
 }
--- src/main/java/com/mmall/pojo/Category.java
+++ src/main/java/com/mmall/pojo/Category.java
@@ -1,5 +1,7 @@
 package com.mmall.pojo;
 
+import com.google.common.base.Objects;
+
 import java.util.Date;
 
 public class Category {
@@ -85,5 +87,20 @@ public class Category {
 
     public void setUpdateTime(Date updateTime) {
         this.updateTime = updateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return id != null ? id.equals(category.id) : category.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
